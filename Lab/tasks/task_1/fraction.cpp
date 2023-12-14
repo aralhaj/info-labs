814b33d42978393878fb0b8f399350b874fb6303
aralhaj <137317021+aralhaj@users.noreply.github.com>
2023-12-14 13:51:29 +0500

    Update fraction.cpp

--- Lab/tasks/task_1/fraction.cpp
+++ Lab/tasks/task_1/fraction.cpp
@@ -1,1 +1,31 @@
+#include "fraction.h"
 
+void fraction::Init(int F, int S)
+{
+    first = F;
+    second = S;
+}
+
+void fraction::Read()
+{
+    cout << "first:";
+    cin >> first;
+    cout << "second:";
+    cin >> second;
+}
+void fraction::Show()
+{
+    cout << "\nfirst=" << first;
+    cout << "\nsecond=" << second;
+    cout << "\n";
+}
+
+int fraction::Power()
+{
+    return pow(first, second);
+}
+
+int fraction::ipart()
+{
+    return (first / second);
+}
